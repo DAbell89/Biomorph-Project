ffd09b6d81100ba0a294a5e0bc0e50e0fc512c66
David Abell <d-abell@live.com>
2016-09-20 09:55:13 -0400

    First Commit

---
+++ Biomorph-Project/Biomorph-Project-Common/BiomorphBaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biomorph_Project_Common
+{
+    public class BiomorphBaseClass
+    {
+        private int tempMin;
+        private int tempMax;
+        private int strengthScore;
+        private int intelScore;
+        private int camoScore;
+        private int visionScore;
+    }
+}
