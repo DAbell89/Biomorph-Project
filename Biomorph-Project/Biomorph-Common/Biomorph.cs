9b07f6f3d4006c33dc1788e42abce26c6d4033e0
David Abell <d-abell@live.com>
2016-10-28 14:03:18 +0000

    Child Generation added

--- Biomorph-Project/Biomorph-Common/Biomorph.cs
+++ Biomorph-Project/Biomorph-Common/Biomorph.cs
@@ -37,7 +37,7 @@ namespace Biomorph.Common
 
         public int Camo
         {
-            get {  return this._camo; }
+            get { return this._camo; }
         }
 
         public int Vision
@@ -48,40 +48,83 @@ namespace Biomorph.Common
         public Biomorph()
         {
             var random = new Random();
-            
+
             _minTemp = random.Next(0, 10);
             _maxTemp = random.Next(0, 10);
             _strength = random.Next(0, 10);
             _intel = random.Next(0, 10);
             _camo = random.Next(0, 10);
             _vision = random.Next(0, 10);
+
+            if (_minTemp > _maxTemp)
+            {
+                _maxTemp = _minTemp + 1;
+            }
         }
 
         /// <summary>
-        /// Calculates the scores of BioMorph vz Opponent and returns a total score
+        /// Initializes a child based of a parent Biomorph
         /// </summary>
-        /// <param name="oponent"></param>
-        /// <returns></returns>
-        public int CombatScore(IBiomorph oponent)
+        /// <param name="parent">Parent Biomorph</param>
+        public Biomorph(Biomorph parent)
         {
-            var scores = new List<int>();
-            scores.Add(Scorer(_strength, oponent.Strength));
-            scores.Add(Scorer(_intel, oponent.Intel));
-            scores.Add(Scorer(_camo, oponent.Camo));
-            scores.Add(Scorer(_vision, oponent.Vision));
+            var random = new Random();
 
-            return scores.Sum();
+            _minTemp = random.Next(parent.MinTemp - 5, parent.MinTemp + 5);
+            _maxTemp = random.Next(parent.MaxTemp - 5, parent.MaxTemp + 5);
+            _strength = random.Next(parent.Strength - 5, parent.Strength + 5);
+            _intel = random.Next(parent.Intel - 5, parent.Intel + 5);
+            _camo = random.Next(parent.Camo - 5, parent.Camo + 5);
+            _vision = random.Next(parent.Vision - 5, parent.Vision + 5);
+
+            if (_minTemp > _maxTemp)
+            {
+                _maxTemp = _minTemp + 1;
+            }
         }
 
         /// <summary>
-        /// Calculates the difference between scores
+        /// Generates 5 offspring for the next generation with mutated states based off the parent
         /// </summary>
-        /// <param name="bioScore">Biomorph's Score</param>
-        /// <param name="oponentScore">Oponent's score</param>
-        /// <returns>Difference between scores</returns>
-        private int Scorer(int bioScore, int oponentScore)
+        /// <returns>5 offspring</returns>
+        public List<Biomorph> GenerateNextGen()
         {
-            return bioScore - oponentScore;
+            var offspring = new List<Biomorph>();
+
+            while (offspring.Count < 5)
+            {
+                offspring.Add(new Biomorph(this));
+            }
+
+
+            return offspring;
         }
+
+    /// <summary>
+    /// Calculates the scores of BioMorph vz Opponent and returns a total score
+    /// </summary>
+    /// <param name="oponent"></param>
+    /// <returns></returns>
+    public int CombatScore(IBiomorph oponent)
+    {
+        var scores = new List<int>();
+        scores.Add(Scorer(_strength, oponent.Strength));
+        scores.Add(Scorer(_intel, oponent.Intel));
+        scores.Add(Scorer(_camo, oponent.Camo));
+        scores.Add(Scorer(_vision, oponent.Vision));
+
+        return scores.Sum();
     }
+
+    /// <summary>
+    /// Calculates the difference between scores
+    /// </summary>
+    /// <param name="bioScore">Biomorph's Score</param>
+    /// <param name="oponentScore">Oponent's score</param>
+    /// <returns>Difference between scores</returns>
+    private int Scorer(int bioScore, int oponentScore)
+    {
+        return bioScore - oponentScore;
+    }
+}
 }
--- Biomorph-Project/Biomorph.Common.Test/BiomorphTests.cs
+++ Biomorph-Project/Biomorph.Common.Test/BiomorphTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Biomorph.Common.Test
 {
@@ -11,6 +12,26 @@ namespace Biomorph.Common.Test
         {
             var bio = new Biomorph();
         }
+        [TestMethod]
+        public void ItGeneratesFiveOffspring()
+        {
+            var bio = new Biomorph();
+            List<Biomorph> nextGen = bio.GenerateNextGen();
+
+            Assert.AreEqual(5, nextGen.Count);
+        }
+
+        [TestMethod]
+        public void MutationsAreInRange()
+        {
+            var parent = new Biomorph();
+            List<Biomorph> nextGen = parent.GenerateNextGen();
+
+            foreach (var child in nextGen)
+            {
+                AssertMainStatsInRangeOfParent(parent, child);
+            }
+        }
 
         [TestMethod]
         public void CombatMethodReturnsTheSumOfTheScoreDifferences()
@@ -18,9 +39,22 @@ namespace Biomorph.Common.Test
             var bio = new Biomorph();
             var opponent = new Biomorph();
 
-            var expected = (bio.Strength - opponent.Strength) + (bio.Intel - opponent.Intel) + (bio.Camo - opponent.Camo) + (bio.Vision - opponent.Vision);
+            var expected = CombatScore(bio, opponent);
 
             Assert.AreEqual(expected, bio.CombatScore(opponent));
+        }
+
+        private static int CombatScore(Biomorph bio, Biomorph opponent)
+        {
+            return (bio.Strength - opponent.Strength) + (bio.Intel - opponent.Intel) + (bio.Camo - opponent.Camo) + (bio.Vision - opponent.Vision);
+        }
+
+        private static void AssertMainStatsInRangeOfParent(Biomorph parent, Biomorph child)
+        {
+            Assert.IsTrue((parent.Strength - 5) <= child.Strength && child.Strength <= (parent.Strength + 5));
+            Assert.IsTrue((parent.Intel - 5) <= child.Intel && child.Intel <= (parent.Intel + 5));
+            Assert.IsTrue((parent.Camo - 5) <= child.Camo && child.Camo <= (parent.Camo + 5));
+            Assert.IsTrue((parent.Vision - 5) <= child.Vision && child.Vision <= (parent.Vision + 5));
         }
     }
 }
