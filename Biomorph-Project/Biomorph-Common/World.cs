5c90b1c2271dc99e5a039b3c43a13f7df3ae5ece
David Abell <d-abell@live.com>
2016-10-31 22:27:11 +0000

    World Added

---
+++ Biomorph-Project/Biomorph-Common/World.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biomorph.Common
+{
+    public class World
+    {
+        private Biomorph _bio;
+        private Biomorph _opponent;
+        private Environment _environment;
+
+        public Biomorph Bio
+        {
+            get
+            {
+                return _bio;
+            }
+
+            set
+            {
+                _bio = value;
+            }
+        }
+
+        public Biomorph Opponent
+        {
+            get
+            {
+                return _opponent;
+            }
+
+            set
+            {
+                _opponent = value;
+            }
+        }
+
+        public Environment Environment
+        {
+            get
+            {
+                return _environment;
+            }
+
+            set
+            {
+                _environment = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of a World
+        /// </summary>
+        public World()
+        {
+            _bio = new Biomorph();
+            _opponent = new Biomorph();
+            _environment = new Environment();
+        }
+    }
+}
---
+++ Biomorph-Project/Biomorph.Common.Test/WorldTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biomorph.Common.Test
+{
+    [TestClass]
+    public class WorldTest
+    {
+        [TestMethod]
+        public void ItInitializesWithBioOpponentAndEnvironment()
+        {
+            var world = new World();
+
+            Assert.AreNotEqual(null, world.Bio);
+            Assert.AreNotEqual(null, world.Opponent);
+            Assert.AreNotEqual(null, world.Environment);
+        }
+    }
+}
